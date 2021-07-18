c800791c65605169f5251958b4bf9b2e03c42c56
William Daniel <wdarchenemy@gmail.com>
2021-07-18 18:33:02 -0300

    Correção na activity AddBusinessCard

--- app/src/main/java/br/com/williamdsk/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/williamdsk/businesscard/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.williamdsk.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.williamdsk.businesscard.databinding.ActivityMainBinding
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        insertListeners()
     }
 
+    private fun insertListeners(){
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
